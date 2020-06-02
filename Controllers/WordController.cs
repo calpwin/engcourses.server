4a6e9a35b69587536ca503ce7fc0bddb9d927fc6
Andrey <calpwin@gmail.com>
2020-06-03 01:24:19 +0300

    dev

---
+++ Controllers/WordController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using EngCourses.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EngCourses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+        [HttpGet("getwords")]
+        public IEnumerable<Word> GetWords()
+        {
+            var wordJson = string.Empty;
+            var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+            using var stream = embeddedProvider.GetFileInfo("Resources/words.json").CreateReadStream();
+            wordJson = new StreamReader(stream).ReadToEnd();
+
+            var words = JsonConvert.DeserializeObject<Word[]>(wordJson);
+
+            return words;
+        }
+    }
+}
---
+++ Models/Word.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace EngCourses.Models
+{
+    public class Word
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("eng")]
+        public string Eng { get; set; }
+
+        [JsonProperty("rus")]
+        public string Rus { get; set; }
+
+        [JsonProperty("enex")]
+        public string Enex { get; set; }
+
+        [JsonProperty("ruex")]
+        public string Ruex { get; set; }
+
+        [JsonProperty("picurl")]
+        public string Picurl { get; set; }
+
+        [JsonProperty("picau")]
+        public string Picau { get; set; }
+
+        [JsonProperty("gap")]
+        public long Gap { get; set; }
+
+        [JsonProperty("engText")]
+        public string EngText { get; set; }
+
+        [JsonProperty("engImage")]
+        public string EngImage { get; set; }
+
+        [JsonProperty("engAudio")]
+        public string EngAudio { get; set; }
+
+        [JsonProperty("engAudioExtend")]
+        public string EngAudioExtend { get; set; }
+    }
+}
